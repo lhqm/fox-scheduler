3fc713ffb6b843e5089a7ed1878e7ad00bd7cb0f
lhqm <1229796683@qq.com>
2023-07-24 16:12:19 +0800

    引入了启动类配置，使得可以通过启动类上添加@EnableFoxScheduler来快速启动一个动态定时任务扫描。

--- fox-client/src/main/java/com/fox/ClientApplication.java
+++ fox-client/src/main/java/com/fox/ClientApplication.java
@@ -1,13 +1,10 @@
 package com.fox;
 
-import com.fox.entity.AnnotatedMethod;
-import com.fox.utils.client.ScheduleTaskScan;
+import com.fox.annotation.EnableFoxScheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author 离狐千慕
@@ -15,17 +12,9 @@ import java.util.List;
  * @date 2023/7/24 13:55
  */
 @SpringBootApplication
+@EnableFoxScheduler
 public class ClientApplication {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args){
         SpringApplication.run(ClientApplication.class,args);
-        List<AnnotatedMethod> methodList = ScheduleTaskScan.processAnnotations(null);
-        methodList.forEach(item->{
-            System.out.println("========================");
-            System.out.println(item.getMethod().getName());
-            System.out.println(Arrays.toString(item.getMethod().getParameterTypes()));
-            System.out.println(item.getMethod().getReturnType().getName());
-            System.out.println("--数值列表--");
-            System.out.println(item.getAnnotationValue());
-        });
     }
 }
