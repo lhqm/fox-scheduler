f08579ee5d8b2e43c3e3ecb0a7d8c601ebeb17aa
lhqm <1229796683@qq.com>
2023-07-25 10:18:22 +0800

    定义编码解码、发送和接收的相关协议。将编码解码器优化到framework模块进行统一管理

---
+++ fox-framework/src/main/java/com/fox/utils/RpcMessageDecoder.java
@@ -0,0 +1,46 @@
+package com.fox.utils;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.CharsetUtil;
+import protocol.RpcMessage;
+
+import java.util.List;
+
+/**
+ * @author 离狐千慕
+ * @version 1.0
+ * @date 2023/7/25 09:57:25
+ * netty解码器
+ */
+public class RpcMessageDecoder extends ByteToMessageDecoder {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() < 4) {
+            // 如果可读字节不足4个，直接返回
+            return;
+        }
+
+        // 标记当前读索引位置
+        in.markReaderIndex();
+        // 读取字节流长度
+        int length = in.readInt();
+
+        if (in.readableBytes() < length) {
+            // 如果可读字节不足消息长度，重置读索引，等待更多数据到达
+            in.resetReaderIndex();
+            return;
+        }
+
+        // 读取字节数据
+        byte[] bytes = new byte[length];
+        in.readBytes(bytes);
+        // 将字节数据转换为JSON字符串
+        String json = new String(bytes, CharsetUtil.UTF_8);
+        // 将JSON字符串转换为RpcMessage对象
+        RpcMessage rpcMessage = RpcMessage.fromJsonString(json);
+        out.add(rpcMessage);
+    }
+}
---
+++ fox-framework/src/main/java/com/fox/utils/RpcMessageEncoder.java
@@ -0,0 +1,27 @@
+package com.fox.utils;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.util.CharsetUtil;
+import protocol.RpcMessage;
+
+/**
+ * @author 离狐千慕
+ * @version 1.0
+ * @date 2023/7/25 09:41:46
+ * netty编码器
+ */
+public class RpcMessageEncoder extends MessageToByteEncoder<RpcMessage> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, RpcMessage rpcMessage, ByteBuf out) throws Exception {
+        // 将RpcMessage对象转换为JSON字符串
+        String json = rpcMessage.toJsonString();
+        // 将JSON字符串转换为字节流
+        byte[] bytes = json.getBytes(CharsetUtil.UTF_8);
+        // 写入字节流长度和字节数据到ByteBuf
+        out.writeInt(bytes.length);
+        out.writeBytes(bytes);
+    }
+}
